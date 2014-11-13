701bc2631746bbf4700dc1348b9fbb0c3ebf8417
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-11-12 21:36:50 -0300

    bundled

--- .bin/views/documentation/complements.js
+++ .bin/views/documentation/complements.js
@@ -11,13 +11,13 @@ jade_debug.unshift({ lineno: 1, filename: "views/documentation/complements.jade"
 buf.push("<section class=\"ly-section md-markdown\">");
 jade_debug.unshift({ lineno: undefined, filename: jade_debug[0].filename });
 jade_debug.unshift({ lineno: 2, filename: "views/documentation/complements.jade" });
-buf.push("<h1 id=\"complementary-modules\">Complementary Modules</h1>\n<p>Foo</p>\n");
+buf.push("<h1 id=\"complementary-modules\">Complementary Modules</h1>\n<p><code>dominus</code>\n<code>xhr</code>\n<code>measly</code></p>\n");
 jade_debug.shift();
 jade_debug.shift();
 buf.push("</section>");
 jade_debug.shift();
 jade_debug.shift();}.call(this,"undefined" in locals_for_with?locals_for_with.undefined:typeof undefined!=="undefined"?undefined:undefined));;return buf.join("");
 } catch (err) {
-  jade.rethrow(err, jade_debug[0].filename, jade_debug[0].lineno, "section.ly-section.md-markdown\n  :markdown\n    # Complementary Modules\n\n    Foo\n");
+  jade.rethrow(err, jade_debug[0].filename, jade_debug[0].lineno, "section.ly-section.md-markdown\n  :markdown\n    # Complementary Modules\n\n    `dominus`\n    `xhr`\n    `measly`\n");
 }
 }
